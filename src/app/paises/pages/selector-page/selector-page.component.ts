20b4c4265f20f7050842440ef5f7a9ea0c49170d
FelipeMarinNRW <felipe.marin@numrot.com>
2022-01-30 10:38:45 -0500

    - Se termina proyecto de selectores.

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { switchMap, tap } from 'rxjs/operators';
+
 import { PaisSmall } from '../../interfaces/paises-interfaces';
 import { PaisesServiceService } from '../../services/paises-service.service';
 
@@ -14,18 +17,24 @@ export class SelectorPageComponent implements OnInit {
   // llenar selectores
   regiones: string[] = [];
   paises: PaisSmall[] = [];
+  //fronteras: string[] = [];
+  fronteras: PaisSmall[] = [];
 
-  constructor(
-    private fb: FormBuilder,
-    private PaisesSerivice: PaisesServiceService
-  ) {}
+  //UI
+  cargando: boolean = false;
+
+  constructor
+    (
+      private fb: FormBuilder,
+      private PaisesSerivice: PaisesServiceService
+    ) { }
 
   ngOnInit(): void {
     this.initializeForm();
     this.regiones = this.PaisesSerivice.regiones
 
     //Cuando cambie la region
-    this.miFormulario.get('region')?.valueChanges
+    /* this.miFormulario.get('region')?.valueChanges
       .subscribe( region => {
         console.log(region);
 
@@ -34,13 +43,52 @@ export class SelectorPageComponent implements OnInit {
             console.log(paises);
             this.paises = paises;
           });
+      }); */
+
+    //Cuando cambie la region
+    this.miFormulario.get('region')?.valueChanges
+      .pipe(
+        tap( ( _ ) => {
+          //console.log(region);
+          this.miFormulario.get('pais')?.reset('');
+          this.cargando = true;
+        }),
+        switchMap(region => this.PaisesSerivice.getPaisesPorRegion(region))
+      )
+      .subscribe(paises => {
+        //console.log(paises);
+        this.paises = paises;
+        this.cargando = false;
       });
+
+    //Cuando cambia el pais
+    this.miFormulario.get('pais')?.valueChanges
+      .pipe(
+        tap( ( _ ) => {
+          this.fronteras = [];
+          this.miFormulario.get('frontera')?.reset('');
+          this.cargando = true;
+        }),
+        switchMap( codigo => this.PaisesSerivice.getPaisPorCodigo(codigo)),
+        switchMap( pais => this.PaisesSerivice.getPaisesPorCodigos( pais == null || undefined ? [""] : pais![0].borders ) )
+      )
+      .subscribe(paises => {
+        if (!paises) {
+          return;
+        } else {
+          //this.fronteras = paises[0].borders;
+          console.log(paises);
+          this.fronteras = paises;
+          this.cargando = false;
+        }
+      })
   }
 
   initializeForm() {
     this.miFormulario = this.fb.group({
       region: ['', Validators.required],
-      pais: ['', Validators.required]
+      pais: ['', Validators.required],
+      frontera: ['', Validators.required]
     });
   }
 
